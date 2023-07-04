b7f2b7ea67870eb9bb5b5b55e7f63d6f1acf4cda
Frederick Javalera <frederickjavalera@gmail.com>
2023-07-04 12:51:25 -0600

    Add project files.

---
+++ ASP.NET_Template.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using ASP.NET_Template.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Template.API.Controllers
+{
+    [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+      _logger.LogInformation("GetWeatherForecast called");
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+  }
+}
